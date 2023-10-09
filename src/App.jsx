bc9c3a8b467d5dc811d97fedbcfae786493801a6
sriman01 <srimankumar45@gmail.com>
2023-10-09 16:50:41 +0530

    fouth commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <TodoState >
-    <div className=' bg-pink-300 h-screen w-full '>
+    <div className=' bg-pink-300 min-h-screen w-full pb-2 '>
       <div className=' flex flex-col items-center pt-10'>
         <div className=' md:text-3xl md:font-bold text-pink-900 md:mb-10 text-xl font-bold mb-5'>To Do List...!</div>
         <AddTodo />
