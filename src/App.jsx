388205a3086f28ca77bcd78e94ae4eb723e699ff
sriman01 <srimankumar45@gmail.com>
2023-10-14 20:48:49 +0530

    message commitee

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <TodoState >
     <div className=' flex items-center justify-center bg-slate-100 min-h-screen w-full pb-2 font-[poppins]'>
-      <div className=' flex flex-col items-center p-10 bg-white shadow-inner w-fit'>
+      <div className=' flex flex-col items-center p-2 sm:p-4 md:p-10 bg-white shadow-inner w-fit'>
         <div 
           className={`flex items-center justify-center bg-[url("https://intellsys-optimizer.b-cdn.net/interviews/978ea909-91ec-49c2-bd69-d494c097d38d/header.jpg")] md:text-3xl md:font-bold text- md:mb-10 text-xl font-bold mb-5 w-[22rem] sm:[25rem] md:w-[40rem] h-[15rem] rounded-lg`}>
              <div className='text-5xl'>
